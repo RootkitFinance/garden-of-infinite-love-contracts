7261d9d39b71885e7d7c4bc5648de5d235fabfa6
rootkitfinancedev <rootkitfinancedev@protonmail.com>
2021-07-08 21:13:55 -0700

    added ERC20 for tests

--- contracts/test/Paired.sol
+++ contracts/test/Paired.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: U-U-U-UPPPPP!!!
 pragma solidity ^0.7.6;
 
-import "../ERC20.sol";
+import "./ERC20.sol";
 
 contract Paired is ERC20("Paired", "PAI") 
 { 
--- contracts/test/RootKit.sol
+++ contracts/test/RootKit.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: U-U-U-UPPPPP!!!
 pragma solidity ^0.7.6;
 
-import "../ERC20.sol";
+import "./ERC20.sol";
 
 contract RootKit is ERC20("RootKit", "ROOT") 
 { 
