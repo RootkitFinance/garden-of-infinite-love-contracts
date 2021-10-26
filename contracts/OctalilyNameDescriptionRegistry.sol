579f79526a3f0d94be0e15bd6b57a30e2dea4f77
billy93 <billyfebram@gmail.com>
2021-10-26 18:44:42 +0700

    Octalily name and description registry

--- contracts/OctalilyNameDescriptionRegistry.sol
+++ contracts/OctalilyNameDescriptionRegistry.sol
@@ -13,7 +13,6 @@ contract OctalilyNameDescriptionRegistry {
     function setName(address octalilyAddress, bytes32 name) public {
         Octalily octalily = Octalily(octalilyAddress);
         require(msg.sender == octalily.owner(), "Owner only");
-        require(names[octalilyAddress] == 0, "Already set");
         names[octalilyAddress] = name;
         emit NameSet(octalilyAddress, name);
     }
