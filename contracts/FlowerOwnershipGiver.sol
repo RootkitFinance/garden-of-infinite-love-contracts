c952bf14b3f07cc65d6acc61bb591051aa9abfa1
rootkitfinancedev <rootkitfinancedev@protonmail.com>
2021-10-30 22:42:32 -0700

    FlowerOwnershipGiver update

--- contracts/FlowerOwnershipGiver.sol
+++ contracts/FlowerOwnershipGiver.sol
@@ -2,14 +2,26 @@
 pragma solidity ^0.7.6;
 
 import "./IMultiOwned.sol";
+import "./Owned.sol";
 
-contract FlowerOwnershipGiver {
+contract FlowerOwnershipGiver is Owned {
     mapping (uint256 => address) public ownedFlowers;
-    mapping (address => bool) public receivers;
+    mapping (address => uint256) public receivedFlowers;
+
     uint256 public availableFlowerIndex;
     uint256 public flowerCount;
+    uint256 public maxAllowedFlowers = 1;
+    uint256 public maxOwnershipIndex = 15;
 
-    function claimFlowerOwnership(IMultiOwned flower) public {
+    function increaseMaxAllowedFlowers() public ownerOnly() {
+        maxAllowedFlowers++;
+    }
+
+    function setMaxOwnershipIndex(uint256 _maxOwnershipIndex) public ownerOnly() {
+        maxOwnershipIndex = _maxOwnershipIndex;
+    }
+
+    function claimFlowerOwnership(IMultiOwned flower) public ownerOnly() {
         flower.claimOwnership();
         ownedFlowers[++flowerCount] = address(flower);
         if (availableFlowerIndex == 0) {
@@ -17,18 +29,31 @@ contract FlowerOwnershipGiver {
         }
     }
 
+    function reclaimFlower(IMultiOwned flower) public ownerOnly() {
+        require (flower.ownerCount() == maxOwnershipIndex, "Ownership slots are still available");
+        flower.transferOwnership(msg.sender);
+    }
+
     function giveMeFlower() public {
-        require (!receivers[msg.sender], "Already received");
+        require (receivedFlowers[msg.sender] < maxAllowedFlowers, "Max allowance reached");
         require (flowerCount > 0, "No flowers available");  
 
         IMultiOwned flower = IMultiOwned(ownedFlowers[availableFlowerIndex]);
 
-        if (flower.ownerCount() == 23) {
+        if (flower.ownerCount() == maxOwnershipIndex) {
             require (++availableFlowerIndex <= flowerCount, "No flowers available");  
             flower = IMultiOwned(ownedFlowers[availableFlowerIndex]);
         }
        
         flower.addExtraOwners(flower.ownerCount() + 1, msg.sender);
-        receivers[msg.sender] = true;
+        receivedFlowers[msg.sender]++;
+    }
+
+    function recoverTokens(IERC20 token) public ownerOnly() {
+        token.transfer(msg.sender, token.balanceOf(address(this)));
+    }
+
+    function recoverChainToken() public ownerOnly() {
+        msg.sender.transfer(address(this).balance);
     }
 }
